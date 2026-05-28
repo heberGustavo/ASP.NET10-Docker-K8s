d26976b96d8a108a88ecb89772fa8511f42efb8d
heberGustavo <heber.gbarbosa@hotmail.com>
2026-05-28 12:21:50 -0300

    Implementing endpoint to Sub, Mult, Div, Mean and Sqrt

--- ASP.NET10-Docker-K8s/Controllers/MathController.cs
+++ ASP.NET10-Docker-K8s/Controllers/MathController.cs
@@ -8,17 +8,61 @@ namespace ASP.NET10_Docker_K8s.Controllers
     public class MathController : ControllerBase
     {
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public ActionResult Get(string firstNumber, string secondNumber)
+        public ActionResult Sum(string firstNumber, string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var sum = ConvertDecimal(firstNumber) + ConvertDecimal(secondNumber);
-                return Ok(sum);
-            }
+                return Ok(ConvertDecimal(firstNumber) + ConvertDecimal(secondNumber));
             
             return BadRequest("Invalid request!");
         }
 
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public ActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                return Ok(ConvertDecimal(firstNumber) - ConvertDecimal(secondNumber));
+
+            return BadRequest("Invalid request!");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public ActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                return Ok(ConvertDecimal(firstNumber) * ConvertDecimal(secondNumber));
+
+            return BadRequest("Invalid request!");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public ActionResult Div(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                return Ok(ConvertDecimal(firstNumber) / ConvertDecimal(secondNumber));
+
+            return BadRequest("Invalid request!");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+                return Ok((ConvertDecimal(firstNumber) + ConvertDecimal(secondNumber)) / 2);
+
+            return BadRequest("Invalid request!");
+        }
+
+        [HttpGet("sqrt/{firstNumber}/{secondNumber}")]
+        public ActionResult Sqrt(string number)
+        {
+            if(IsNumeric(number))
+                return Ok(Math.Sqrt(ConvertDouble(number)));
+
+            return BadRequest("Invalid request!");
+        }
+
+        #region Private Methods 
+        
         private bool IsNumeric(string strNumber)
         {
             decimal decimalValue;
@@ -49,5 +93,24 @@ namespace ASP.NET10_Docker_K8s.Controllers
 
             return 0;
         }
+
+        private double ConvertDouble(string strNumber)
+        {
+            double doubleValue;
+
+            if (double.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out doubleValue
+            ))
+            {
+                return doubleValue;
+            }
+
+            return 0;
+        }
+
+        #endregion
     }
 }
