03ed41ba5d1e53c9382435be88d5fa161f583f32
heberGustavo <heber.gbarbosa@hotmail.com>
2026-05-28 12:07:42 -0300

    Creating Math Controller to implement all type of the Calculator

---
+++ ASP.NET10-Docker-K8s/Controllers/MathController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET10_Docker_K8s.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public ActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertDecimal(firstNumber) + ConvertDecimal(secondNumber);
+                return Ok(sum);
+            }
+            
+            return BadRequest("Invalid request!");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            decimal decimalValue;
+
+            bool isNumber = decimal.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+            );
+
+            return isNumber;
+        }
+
+        private decimal ConvertDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+            ))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+    }
+}
