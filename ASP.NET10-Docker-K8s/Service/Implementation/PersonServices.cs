6473edebb1d07915d31fe99c22224b32c5b4d3e7
heberGustavo <heber.gbarbosa@hotmail.com>
2026-05-29 18:11:30 -0300

    Updating methods to use EF Core in methods: Create, Update, Delete, FindAll and FindById

--- ASP.NET10-Docker-K8s/Service/Implementation/PersonServices.cs
+++ ASP.NET10-Docker-K8s/Service/Implementation/PersonServices.cs
@@ -1,50 +1,54 @@
 ﻿using ASP.NET10_Docker_K8s.Model;
+using ASP.NET10_Docker_K8s.Model.Context;
 using ASP.NET10_Docker_K8s.Service.Interface;
 
 namespace ASP.NET10_Docker_K8s.Service.Implementation
 {
     public class PersonServices : IPersonServices
     {
+        private MSSQLContext _context;
+
+        public PersonServices(MSSQLContext context)
+        {
+            _context = context;
+        }
+
         public List<Person> FindAll()
         {
-            var person = new List<Person>();
-
-            for(int i = 1; i <= 10; i++)
-            {
-                person.Add(MockPerson(i));
-            }
-
-            return person;
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
-            return MockPerson(id);
+            return _context.Persons.Find(id);
         }
 
         public Person Create(Person person)
         {
-            person.Id = new Random().Next(1, 1000);
+            _context.Persons.Add(person);
+            _context.SaveChanges();
             return person;
         }
 
         public Person Update(Person person)
         {
+            var personFound = FindById(person.Id);
+            if(personFound == null) return null;
+
+            _context.Entry(personFound).CurrentValues.SetValues(person);
+            _context.SaveChanges();
             return person;
         }
 
         public void Delete(long id)
         {
+            var personFound = FindById(id);
+            if (personFound == null) return;
+
+            _context.Persons.Remove(personFound);
+            _context.SaveChanges();
             return;
         }
 
-        #region Private Methods
-
-        private Person MockPerson(long id)
-        {
-            return new Person(id, $"Person {id}", $"LastName {id}", $"City {id} - SP", "Male");
-        }
-
-        #endregion
     }
 }
